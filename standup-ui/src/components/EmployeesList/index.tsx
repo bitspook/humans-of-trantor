9201d4c46f6e1fc4d0af00f10623c2df7abacfe1
Charanjit Singh <charanjit.singh@trantorinc.com>
2019-11-26 18:49:38 +0000

    feat: start dev version of standup-ui as a part of `dc up`
    - Run the cloud on Port 700X because I keep it running in background during the
    day and 700x ports don't collide with other services

--- standup-ui/src/components/EmployeesList/index.tsx
+++ standup-ui/src/components/EmployeesList/index.tsx
@@ -3,37 +3,59 @@ import { Image, List } from 'semantic-ui-react';
 import { Employee } from '../../ducks/employees';
 import './index.module.scss';
 
-const EmployeeListItem = (c: Employee) => {
-  const avatarImage = c.avatar || '/images/default-employee.png';
+interface EmployeeListItemProps {
+  employee: Employee;
+  onClick: (e: Employee) => void;
+  isHighlighted: boolean;
+}
+const EmployeeListItem = ({
+  employee,
+  onClick,
+  isHighlighted
+}: EmployeeListItemProps) => {
+  const avatarImage = employee.avatar || '/images/default-employee.png';
 
   return (
-    <List.Item key={c.eCode}>
+    <List.Item onClick={() => onClick(employee)} active={isHighlighted}>
       <Image avatar={true} src={avatarImage} />
       <List.Content>
-        <List.Header>
-          {c.name}
-        </List.Header>
-        <List.Description>
-          +{c.eCode}
-        </List.Description>
+        <List.Header>{employee.name}</List.Header>
+        <List.Description>+{employee.ecode}</List.Description>
       </List.Content>
     </List.Item>
   );
 };
 
-interface IEmployeesListDataProps {
+interface DataProps {
   employees: Employee[];
+  selectedEmployee: string;
 }
 
-interface IEmployeesListCbProps {
+interface CbProps {
+  onSelect: (e: Employee) => void;
 }
 
-const EmployeesList: React.FC<IEmployeesListDataProps & IEmployeesListCbProps> = ({
+const EmployeesList: React.FC<DataProps & CbProps> = ({
   employees,
+  onSelect,
+  selectedEmployee
 }) => {
   return (
-    <List selection={true} relaxed={true} divided={true} verticalAlign='middle'>
-      {employees.map(EmployeeListItem)}
+    <List
+      selection={true}
+      relaxed={true}
+      divided={true}
+      verticalAlign="middle"
+      size="big"
+    >
+      {employees.map(e => (
+        <EmployeeListItem
+          employee={e}
+          key={e.ecode}
+          onClick={onSelect}
+          isHighlighted={e.ecode === selectedEmployee}
+        />
+      ))}
     </List>
   );
 };
---
+++ standup-ui/src/components/StandupForm/index.tsx
@@ -0,0 +1,142 @@
+import fecha from 'fecha';
+import {
+  Field,
+  FieldProps,
+  Formik,
+  FormikHelpers,
+  FormikProps,
+  getIn
+} from 'formik';
+import React from 'react';
+import { Button, Form, Input, TextArea } from 'semantic-ui-react';
+import * as yup from 'yup';
+import c from './index.module.scss';
+
+export interface StandupFormValues {
+  delivered: {
+    date: string;
+    standup: string;
+  };
+  committed: {
+    date: string;
+    standup: string;
+  };
+}
+
+const StandupSchema = yup.object().shape({
+  delivered: yup
+    .object()
+    .shape({
+      date: yup.date().required('Delivered date is required'),
+      standup: yup.string().required('Standup is required')
+    })
+    .required(),
+  committed: yup
+    .object()
+    .shape({
+      date: yup.date().required('Committed date is required'),
+      standup: yup.string().required('Standup is required')
+    })
+    .required()
+});
+
+const today = new Date();
+const yesterday = new Date().setDate(today.getDate() - 1);
+const emptyContact: StandupFormValues = {
+  delivered: {
+    date: fecha.format(yesterday, 'YYYY-MM-DD'),
+    standup: ''
+  },
+  committed: {
+    date: fecha.format(today, 'YYYY-MM-DD'),
+    standup: ''
+  }
+};
+
+interface SemanticFieldProps {
+  label: string;
+  type?: string;
+  fluid?: boolean;
+}
+
+const SemanticInputField = (props: FieldProps & SemanticFieldProps) => {
+  const error =
+    getIn(props.form.touched, props.field.name) &&
+    getIn(props.form.errors, props.field.name);
+
+  return (
+    <Form.Field error={Boolean(error)}>
+      <label>{props.label}</label>
+      <Input {...props.field} fluid={props.fluid} type={props.type} />
+      {error && <span className={c.error}>{error}</span>}
+    </Form.Field>
+  );
+};
+
+const SemanticTextAreaField = (props: FieldProps & SemanticFieldProps) => {
+  const error =
+    getIn(props.form.touched, props.field.name) &&
+    getIn(props.form.errors, props.field.name);
+
+  return (
+    <Form.Field error={Boolean(error)}>
+      <label>{props.label}</label>
+      <TextArea {...props.field} fluid={props.fluid} />
+      {error && <span className={c.error}>{error}</span>}
+    </Form.Field>
+  );
+};
+
+const InnerForm: React.FC<FormikProps<StandupFormValues>> = props => (
+  <Form onSubmit={props.handleSubmit}>
+    <h2>Delivered</h2>
+    <Field
+      component={SemanticInputField}
+      name="delivered.date"
+      label="Date"
+      type="date"
+    />
+    <Field
+      component={SemanticTextAreaField}
+      name="delivered.standup"
+      label="Standup"
+    />
+
+    <h2>Commit</h2>
+    <Field
+      component={SemanticInputField}
+      name="committed.date"
+      label="Date"
+      type="date"
+    />
+    <Field
+      component={SemanticTextAreaField}
+      name="committed.standup"
+      label="Standup"
+    />
+
+    <Button primary={true} type="submit">
+      Save
+    </Button>
+  </Form>
+);
+
+interface StandupFormCbProps {
+  onSave: (
+    values: StandupFormValues,
+    helpers: FormikHelpers<StandupFormValues>
+  ) => void;
+}
+
+const NewContact: React.FC<StandupFormCbProps> = ({ onSave }) => {
+  return (
+    <Formik
+      initialValues={emptyContact}
+      validationSchema={StandupSchema}
+      onSubmit={onSave}
+      component={InnerForm}
+    />
+  );
+};
+
+export default NewContact;
