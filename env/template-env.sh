6886c6e411b0c258d7700bd4d898f8dfb96ddc04
Charanjit Singh <charanjit.singh@trantorinc.com>
2019-12-20 18:16:16 +0530

    feat: add role to deploy hot-cloud

--- env/template-env.sh
+++ env/template-env.sh
@@ -1,4 +1,11 @@
-HOT_VERSION="0.0.1"
-DOCKER_REGISTRY="registry.hub.docker.com/channikhabra"
-DOCKER_REGISTRY_USERNAME="channikhabra"
-DOCKER_REGISTRY_PASSWORD=""
+export DOCKER_REGISTRY="registry.hub.docker.com/channikhabra"
+export DOCKER_REGISTRY_USERNAME="channikhabra"
+export DOCKER_REGISTRY_PASSWORD=""
+
+export HOT_VERSION=""
+
+export HOT_DB_HOST=""
+export HOT_DB_PORT=""
+export HOT_DB_NAME=""
+export HOT_DB_USER=""
+export HOT_DB_PASSWORD=""
