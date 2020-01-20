eda60795ca1fb4b63519f5a2970646a6d0d717d2
Charanjit Singh <charanjit.singh@trantorinc.com>
2020-01-20 15:18:43 +0530

    fix: treat Session API types more like types and less like API

--- iam/src/Session.hs
+++ iam/src/Session.hs
@@ -5,25 +5,15 @@ module Session
 where
 
 
-
-import           Servant                       as S
+import           Data.Pool
+import           Data.Text.Encoding         (decodeUtf8)
+import           Database.PostgreSQL.Simple
+import           RIO
+import           RIO.ByteString.Lazy        (toStrict)
+import           Servant                    as S
 import           Servant.Auth.Server
 import           Session.Types
 import           Types
-import           Data.Pool
-import           Database.PostgreSQL.Simple
-import           Data.Text.Encoding             ( decodeUtf8 )
-import           RIO.ByteString.Lazy            ( toStrict )
-import           RIO
-import           Data.Aeson
-
-newtype SessionOpInput = SessionOpInput
-  { refreshToken :: RefreshToken } deriving (Show, Generic, FromJSON)
-
-data NewSessionInput = NewSessionInput
-  { email    :: Email
-  , password :: Text
-  } deriving (Show, Generic, FromJSON)
 
 refreshSession
   :: JWTSettings
@@ -31,10 +21,10 @@ refreshSession
   -> AccessToken
   -> SessionOpInput
   -> S.Handler Session
-refreshSession jwts conns (AccessToken aId aEmail) (SessionOpInput rt) = do
+refreshSession jwts conns (AccessToken aId) (SessionOpInput rt) = do
   v <- liftIO . withResource conns $ \conn -> execute conn "SELECT 1" ()
   traceShowIO v
-  token <- liftIO $ makeJWT (AccessToken aId aEmail) jwts Nothing
+  token <- liftIO $ makeJWT (AccessToken aId) jwts Nothing
   case token of
     Left  e -> throwError err500 { errBody = fromString . show $ e }
     Right t -> return $ Session (decodeUtf8 . toStrict $ t) rt
@@ -50,17 +40,11 @@ secureServer _    _     _                 = throwAll err401
 createSession
   :: JWTSettings -> Pool Connection -> NewSessionInput -> S.Handler Session
 createSession jwts conns input = do
-  _ <- liftIO . withResource conns $ \conn -> execute conn "SELECT 1" ()
-  token <- liftIO $ makeJWT (AccessToken (UUID "1") (email input)) jwts Nothing
+  _     <- liftIO . withResource conns $ \conn -> execute conn "SELECT 1" ()
+  token <- liftIO $ makeJWT (AccessToken (UUID "1")) jwts Nothing
   case token of
     Left  e -> throwError err500 { errBody = fromString . show $ e }
     Right t -> return $ Session (decodeUtf8 $ toStrict t) (RefreshToken "lol")
-
-type SecureAPI
-  = "session" :> "refresh" :> ReqBody '[JSON] SessionOpInput :> Post '[JSON] Session
-type InsecureAPI
-  = "session" :> ReqBody '[JSON] NewSessionInput :> Post '[JSON] Session
-type API auths = (Auth auths AccessToken :> SecureAPI) :<|> InsecureAPI
 
 insecureServer :: JWTSettings -> Pool Connection -> Server InsecureAPI
 insecureServer = createSession
--- iam/src/Session/Types.hs
+++ iam/src/Session/Types.hs
@@ -1,18 +1,13 @@
-module Session.Types
-  ( AccessToken (AccessToken)
-  , RefreshToken (RefreshToken)
-  , Session (Session)
-  )
-where
+module Session.Types where
 
 import           Data.Aeson
 import           RIO
 import           Servant.Auth.Server
 import           Types
+import           Servant
 
-data AccessToken = AccessToken
+newtype AccessToken = AccessToken
   { id    :: UUID
-  , email :: Email
   } deriving (Show, Generic, ToJSON, FromJSON, ToJWT, FromJWT)
 
 newtype RefreshToken = RefreshToken String deriving (Show, Generic, ToJSON, FromJSON)
@@ -21,3 +16,17 @@ data Session = Session
   { accessToken  :: Text
   , refreshToken :: RefreshToken
   } deriving (Show, Generic, ToJSON, FromJSON, ToJWT, FromJWT)
+
+newtype SessionOpInput = SessionOpInput
+  { refreshToken :: RefreshToken } deriving (Show, Generic, FromJSON)
+
+data NewSessionInput = NewSessionInput
+  { email    :: Email
+  , password :: Text
+  } deriving (Show, Generic, FromJSON)
+
+type SecureAPI
+  = "session" :> "refresh" :> ReqBody '[JSON] SessionOpInput :> Post '[JSON] Session
+type InsecureAPI
+  = "session" :> ReqBody '[JSON] NewSessionInput :> Post '[JSON] Session
+type API auths = (Auth auths AccessToken :> SecureAPI) :<|> InsecureAPI
