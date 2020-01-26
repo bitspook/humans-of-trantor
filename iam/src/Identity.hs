139ade2204a5f2dae86d661b99214c2b2fff1b8d
Charanjit Singh <charanjit.singh@trantorinc.com>
2020-01-26 16:56:44 +0530

    fix: handle email conflict on register user

--- iam/src/Identity.hs
+++ iam/src/Identity.hs
@@ -6,23 +6,29 @@ where
 
 import           Data.Pool
 import           Data.Text
-import           Data.Text.Encoding         (decodeUtf8)
+import           Data.UUID                      ( UUID )
+import           Data.Text.Encoding             ( decodeUtf8 )
 import           Database.PostgreSQL.Simple
+import           Database.PostgreSQL.Simple.Errors
 import           Identity.Types
-import           RIO                        hiding (Identity)
-import           Servant                    as S
+import           RIO                     hiding ( Identity )
+import           Servant                       as S
 import           Servant.Auth.Server
 import           Types
 
 registerIdentity :: Pool Connection -> NewIdentityPayload -> S.Handler Identity
 registerIdentity pool (NewIdentityPayload (Email email') password') = do
-  rows <- liftIO . withResource pool $ \conn -> query
-    conn
-    "INSERT INTO iam.identity (email, password) VALUES (?, crypt(?, gen_salt('bf', 10))) RETURNING id, email"
-    (toLower email', password')
+  rows :: Either SqlError [(UUID, ByteString)] <-
+    liftIO $ try $ withResource pool $ \conn -> query
+      conn
+      "INSERT INTO iam.identity (email, password) VALUES (?, crypt(?, gen_salt('bf', 10))) RETURNING id, email"
+      (toLower email', password')
   case rows of
-    (x : _) -> return (Identity (fst x) (Email $ decodeUtf8 . snd $ x))
-    []      -> throwError err400
+    Right (x : _) -> return (Identity (fst x) (Email $ decodeUtf8 . snd $ x))
+    Right _       -> throwError err400
+    Left  e       -> case constraintViolation e of
+      Just (UniqueViolation _) -> throwError err409
+      _                        -> throwError err500
 
 insecureServer :: JWTSettings -> Pool Connection -> Server InsecureAPI
 insecureServer _ = registerIdentity
