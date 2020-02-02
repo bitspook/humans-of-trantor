916934f1fa903655025acd773f98003263d5b74e
Charanjit Singh <charanjit.singh@trantorinc.com>
2020-02-02 15:13:46 +0530

    feat: styles for login form

--- ui/src/views/Login/index.tsx
+++ ui/src/views/Login/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import c from './index.module.scss';
 
 const Login: React.FC = () => {
   return (
-    <h1>Login</h1>
+    <div className={c.container}>
+      <div className={c.formContainer}>
+        <h1 className={c.title}>Humans of Trantor</h1>
+        <form className={c.form}>
+          <input placeholder="E-mail address" />
+          <input placeholder="Password" type="password" />
+
+          <button className={c.loginButton}>Login</button>
+        </form>
+      </div>
+    </div>
   );
 };
 
