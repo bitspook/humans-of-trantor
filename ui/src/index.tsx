c4cc15389926ea376fc49af37b63c7ea548edefd
Charanjit Singh <charanjit.singh@trantorinc.com>
2020-02-09 18:38:23 +0530

    feat: persist authenticated user across refreshes
    Using redux-presist, but only for persisting authenticated user. I tried to
    persist everything, but we are making the assumption in a few places that dates
    in state are going to be day.js objects. Rehyderating from redux-persist breaks
    the app because of this. I am thinking about making a few changes in the app's
    architecture, which will take this concept (of having class instances in state)
    further. Because of this, I am limiting the use of redux-persist.

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Route } from 'react-router';
+import { PersistGate } from 'redux-persist/integration/react';
 import 'semantic-ui-css/semantic.min.css';
 
 import configureStore from 'src/configureStore';
@@ -11,13 +12,15 @@ import 'src/index.scss';
 import App from 'src/views/App';
 
 const history = createBrowserHistory();
-const store = configureStore(history);
+const { store, persistor } = configureStore(history);
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Route path="/" component={App} />
-    </ConnectedRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <Route path="/" component={App} />
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
