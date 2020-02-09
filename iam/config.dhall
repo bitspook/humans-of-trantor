592081c96a2fa76b3eb245e70e1a1a63fc26f7fd
Charanjit Singh <charanjit.singh@trantorinc.com>
2020-02-09 17:14:50 +0530

    feat: add cors-middlware in iam
    - Also make migrations-dir configurable

--- iam/config.dhall
+++ iam/config.dhall
@@ -1,4 +1,5 @@
-{ dbUrl = "postgresql://hot:hot@store:5432/iam"
+{ dbUrl = env:DB_URL ? "postgresql://hot:hot@localhost:5432/iam"
 , port = env:PORT ? 7000
-, jwtKeysPath = "./jwt-keys.pem"
+, jwtKeysPath = "jwt-keys.pem"
+, migrationsDir = "migrations"
 }
--- iam/src/Lib.hs
+++ iam/src/Lib.hs
@@ -7,27 +7,32 @@ module Lib
   ( runApp
   )
 where
-import           Crypto.JOSE                as Jose
-import           Data.Either.Combinators    (fromRight')
+import           Crypto.JOSE                 as Jose
+import           Data.Either.Combinators     (fromRight')
 import           Data.Pool
-import           Database.PostgreSQL.Simple (Connection)
-import           Network.Wai.Handler.Warp   (run)
+import           Database.PostgreSQL.Simple  (Connection)
+import           Network.Wai.Handler.Warp    (run)
 import           RIO
+import           RIO.Text                    (unpack)
 import           Servant
-import           Servant.Auth.Server        as SAS
+import           Servant.Auth.Server         as SAS
 
-import           CryptoUtil                 (readPemRsaKey)
-import           Db                         (initConnectionPool, migrate)
+import           CryptoUtil                  (readPemRsaKey)
+import           Db                          (initConnectionPool, migrate)
 import           Dhall
-import qualified Identity                   (API, server)
-import qualified Session                    (API, server)
+import qualified Identity                    (API, server)
+import           Network.Wai                 (Middleware)
+import           Network.Wai.Middleware.Cors (cors, corsRequestHeaders,
+                                              simpleCorsResourcePolicy)
+import qualified Session                     (API, server)
 
 type API auths = Identity.API :<|> Session.API auths
 
 data Config = Config
-  { dbUrl       :: Text,
-    jwtKeysPath :: Text,
-    port        :: Natural
+  { dbUrl         :: Text,
+    jwtKeysPath   :: Text,
+    migrationsDir :: Text,
+    port          :: Natural
   } deriving (Generic, Show)
 
 instance Interpret Config
@@ -35,12 +40,15 @@ instance Interpret Config
 server :: CookieSettings -> JWTSettings -> Pool Connection -> Server (API auths)
 server c j p = Identity.server c j p :<|> Session.server c j p
 
+corsMiddleware :: Middleware
+corsMiddleware = cors ( const $ Just (simpleCorsResourcePolicy  { corsRequestHeaders = ["Content-Type"] }) )
+
 runApp :: IO ()
 runApp = do
   conf       <- input auto "./config.dhall"
   pool       <- initConnectionPool . encodeUtf8 . dbUrl $ conf
-  _          <- withResource pool $ migrate "migrations"
-  privateKey <- fromRSA . fromRight' <$> readPemRsaKey (show . jwtKeysPath $ conf)
+  _          <- withResource pool $ migrate (unpack . migrationsDir $ conf)
+  privateKey <- fromRSA . fromRight' <$> readPemRsaKey (unpack . jwtKeysPath $ conf)
   let jwtCfg = JWTSettings privateKey
                            (Just Jose.RS256)
                            (Jose.JWKSet [privateKey])
@@ -48,4 +56,4 @@ runApp = do
       cfg = defaultCookieSettings :. jwtCfg :. EmptyContext
       api = Proxy :: Proxy (API '[JWT])
   run (fromIntegral $ port conf)
-    $ serveWithContext api cfg (server defaultCookieSettings jwtCfg pool)
+    $ corsMiddleware $ serveWithContext api cfg (server defaultCookieSettings jwtCfg pool)
