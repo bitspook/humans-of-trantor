fa21cab72c08effb28f109cdac427cc86c7605c4
Charanjit Singh <charanjit.singh@trantorinc.com>
2020-02-27 16:39:54 +0530

    feat: proper date type in ReceivedStandupUpdateV2

--- hot-core/src/Types.hs
+++ hot-core/src/Types.hs
@@ -6,9 +6,12 @@
 
 module Types where
 
-import           Data.Aeson                           (FromJSON, ToJSON)
+import           Data.Aeson
 import           Data.Pool
+import           Data.Text                            as T
 import           Data.Text.Encoding                   (decodeUtf8)
+import           Data.Time.Format
+import           Data.Time.LocalTime
 import           Database.PostgreSQL.Simple           (Connection)
 import           Database.PostgreSQL.Simple.FromField
 import           Database.PostgreSQL.Simple.Internal  (Conversion, Field)
@@ -37,13 +40,26 @@ instance ToField Email where
 ---
 
 -- Date
-newtype Date = Date Text deriving (Show, Generic, FromJSON, ToJSON)
+newtype Date = Date LocalTime deriving (Show, Generic)
+
+instance FromJSON Date where
+  parseJSON (String d) = case parseTimeM False defaultTimeLocale "%Y-%m-%d" (T.unpack d) of
+    Just t  -> return $ Date t
+    Nothing -> fail "Invalid Date. Date should be in format 'YYYY-MM-DD'"
+  parseJSON _ = fail "Invalid Date. Date should be a string in format 'YYYY-MM-DD'"
+
+instance ToJSON Date where
+  toJSON (Date d) = fromString $ formatTime defaultTimeLocale "%Y-%m-%d" d
 
 instance ToField Date where
-  toField (Date d) = Escape $ encodeUtf8 d
+  toField (Date d) = Escape $ fromString $ formatTime defaultTimeLocale "%Y-%m-%d" d
 
 instance FromField Date where
-  fromField = fromTextField Date
+  fromField f dat = case dat of
+    Just b  -> case parseTimeM False defaultTimeLocale "%Y-%m-%d" $ T.unpack $ decodeUtf8 b of
+      Just t  -> return $ Date t
+      Nothing -> returnError ConversionFailed f (show dat)
+    Nothing -> returnError ConversionFailed f (show dat)
 ---
 
 data Config = Config
